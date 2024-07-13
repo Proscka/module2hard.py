96cc5327b2bb536b3908a5a380139c5d053815c7
Proscka <proskinaviktoria861@gmail.com>
2024-07-13 18:24:21 +0300

    Add files via upload

---
+++ module2hard.py/main.py
@@ -0,0 +1,30 @@
+import random
+def stonec():
+    num=random.randit=(3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+    win = random. choice(num)
+    return win
+    win = stonec()
+    print(win)
+    a=[]
+    for i in range(1,num+1):
+        for j in range(1,num+1):
+            num % (i + j)==0
+            i<j
+            i=j
+            print(i<j,i=j)
+            a=[]
+            num=input()
+            for i in num:
+                if num not in a:
+                    a.append(num)
+                num=input()
+            print(num(a))
+
+
+
+
+
+
+
+
+
